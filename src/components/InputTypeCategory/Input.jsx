84b6d8022e66ac32548337ebb5bc8d51c78f1f3d
Denis Ouma <dennisouma38@gmail.com>
2023-04-22 04:55:36 +0300

    Updating the navbar component

--- src/components/InputTypeCategory/Input.jsx
+++ src/components/InputTypeCategory/Input.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import className from "classnames";
 export const Input = ({ inputType, placeholder, ...rest }) => {
 	const classes = className(
-		"placeholder:italic placeholder:text-slate-400 block bg-white  border border-gray-text-color rounded-[4px] py-1  pr-3 shadow-sm focus:outline-none sm:text-sm",
+		" placeholder:text-slate-400 block bg-white  border border-gray-text-color rounded-[4px] py-1  pr-3 shadow-sm focus:outline-none sm:text-sm",
 		rest.className
 	);
 	return (
--- src/components/NavBar/PrimeNavbar.jsx
+++ src/components/NavBar/PrimeNavbar.jsx
@@ -3,20 +3,32 @@ import React from "react";
 import { Logo } from "../JumiaLogo/Logo";
 import { PageContainerPosition } from "../PageContainerPosition";
 import { Icon } from "@iconify/react";
+import { Button } from "../Button";
 export const PrimeNavbar = () => {
 	return (
 		<div className="bg-primary-page-color shadow-lg">
 			<PageContainerPosition>
 				<Logo />
-				<div className="relative">
-					<span className="absolute inset-y-0 left-0 flex items-center pl-2">
-						<Icon icon="ic:baseline-search" color="#75757A" height="20" />
-					</span>
-					<Input
-						inputType="text"
-						placeholder="Search products, brands and categories"
-						className="w-[30rem] pl-7 "
-					/>
+				<div className="flex items-center justify-between">
+					<div className="relative">
+						<span className="absolute inset-y-0 left-0 flex items-center pl-2">
+							<Icon icon="ic:baseline-search" color="#75757A" height="20" />
+						</span>
+						<Input
+							inputType="text"
+							placeholder="Search products, brands and categories"
+							className="w-[30rem] pl-7 "
+						/>
+					</div>
+					<Button
+						search
+						className="shadow-lg shadow-gray-text-color-[500] uppercase border rounded-[5px]"
+					>
+						Search
+					</Button>
+					<Button className="">Account</Button>
+					<Button className="">Help</Button>
+					<Button className="">Cart</Button>
 				</div>
 			</PageContainerPosition>
 		</div>
