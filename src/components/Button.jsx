aec4fc2c25c0b3f1aefa289a093ab6cfd8f0d5cc
Denis Ouma <dennisouma38@gmail.com>
2023-04-22 05:14:16 +0300

    Prime navigation bar completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import className from "classnames";
 export const Button = ({ children, primary, search, ...rest }) => {
 	const classes = className(
-		"flex px-4 text-[12px] ml-[5px] items-center py-1.5  font-[400]",
+		"flex px-4 text-[12px] ml-[5px] items-end py-1.5  font-[400]",
 		{
 			"bg-primary-color-page text-white ": search,
 		},
--- src/components/NavBar/PrimeNavbar.jsx
+++ src/components/NavBar/PrimeNavbar.jsx
@@ -4,6 +4,7 @@ import { Logo } from "../JumiaLogo/Logo";
 import { PageContainerPosition } from "../PageContainerPosition";
 import { Icon } from "@iconify/react";
 import { Button } from "../Button";
+
 export const PrimeNavbar = () => {
 	return (
 		<div className="bg-primary-page-color shadow-lg">
@@ -17,7 +18,7 @@ export const PrimeNavbar = () => {
 						<Input
 							inputType="text"
 							placeholder="Search products, brands and categories"
-							className="w-[30rem] pl-7 "
+							className="w-[24rem] pl-7 "
 						/>
 					</div>
 					<Button
@@ -26,9 +27,45 @@ export const PrimeNavbar = () => {
 					>
 						Search
 					</Button>
-					<Button className="">Account</Button>
-					<Button className="">Help</Button>
-					<Button className="">Cart</Button>
+					<Button className="">
+						<Icon
+							icon="clarity:avatar-line"
+							color="#313133"
+							height="25"
+							className="pr-[3px]"
+						/>
+						Account
+						<Icon
+							icon="material-symbols:keyboard-arrow-down-rounded"
+							color="#313133"
+							height="20"
+							className="px-[3px]"
+						/>
+					</Button>
+					<Button className="">
+						<Icon
+							icon="material-symbols:help-outline"
+							color="#313133"
+							height="25"
+							className="px-[3px]"
+						/>
+						Help
+						<Icon
+							icon="material-symbols:keyboard-arrow-down-rounded"
+							color="#313133"
+							height="20"
+							className="px-[3px]"
+						/>
+					</Button>
+					<Button className="">
+						<Icon
+							icon="ic:outline-shopping-cart"
+							color="#313133"
+							height="25"
+							className="px-[3px]"
+						/>
+						Cart
+					</Button>
 				</div>
 			</PageContainerPosition>
 		</div>
