0ac60cc797694c471b98d4bb768772d965d4aa7e
Denis Ouma <dennisouma38@gmail.com>
2023-04-24 14:12:50 +0300

    Complete design components

--- src/components/AddsComponents/FlashSalesSliderAdd.jsx
+++ src/components/AddsComponents/FlashSalesSliderAdd.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import { AddsSalesTitle } from "../AddsSalesTitle";
 import AddsTopTitleDescription from "../AddsTopTitleDescription";
 import { Icon } from "@iconify/react";
+import { Button } from "../Button";
 function FlashSalesSliderAdd() {
 	return (
 		<div className="w-full mt-4 border-rounded">
 			<AddsSalesTitle>
-				<AddsTopTitleDescription className="bg-danger-add-color ">
-					<div className="flex text-40">
+				<AddsTopTitleDescription className="bg-danger-add-color align-middle font-normal text-[1.2rem] capitalise">
+					<div className="flex items-center text-40">
 						<Icon icon="ion:pricetags-sharp" color="#f90" height="25" />
-						<h2 className="ml-2">Flash Sales</h2>
+						<span className="ml-2">Flash Sales</span>
 					</div>
-					<div className="">Flash Sales</div>
-					<div className="">Flash Sales</div>
+					<div className="">Time Left</div>
+
+					<Button className="uppercase">
+						See All
+						<Icon icon="material-symbols:chevron-right-rounded" height="25" />
+					</Button>
 				</AddsTopTitleDescription>
 				{/* Images sliders goes here */}
 				Images
