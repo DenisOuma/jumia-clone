b1eeb6360f6624fc1af3d7eca7cb073bbb711544
Denis Ouma <dennisouma38@gmail.com>
2023-04-24 23:39:00 +0300

    Transition zoom on hover

--- src/components/StaticAddComponets/StatticCard.jsx
+++ src/components/StaticAddComponets/StatticCard.jsx
@@ -4,8 +4,9 @@ export const StatticCard = ({ images }) => {
 		return (
 			<div key={images.url} className="w-[24rem] rounded">
 				<img
-					className="w-full h-full p-2 mt-0 rounded-[5px]"
+					className="w-full h-full p-2 mt-0 rounded-[5px] object-cover transition duration-500 ease-in-out transform hover:scale-105"
 					src={image.url}
+					style={{ borderRadius: "10px" }}
 					alt="Add "
 				/>
 			</div>
