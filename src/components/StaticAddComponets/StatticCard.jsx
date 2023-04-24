78d803c9834274c886f1c1d6cc5e0ebbd745845e
Denis Ouma <dennisouma38@gmail.com>
2023-04-24 23:46:04 +0300

    Minore changes made

--- src/components/StaticAddComponets/StatticCard.jsx
+++ src/components/StaticAddComponets/StatticCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export const StatticCard = ({ images }) => {
 	const addImages = images.map((image) => {
 		return (
-			<div key={images.url} className="w-[24rem] rounded">
+			<div key={image.id} className="w-[24rem] rounded">
 				<img
 					className="w-full h-full p-2 mt-0 rounded-[5px] object-cover transition duration-500 ease-in-out transform hover:scale-105"
 					src={image.url}
--- src/utils/statciAddDisplay1.js
+++ src/utils/statciAddDisplay1.js
@@ -1,5 +1,5 @@
 export const staticAddDisplay = [
-	{ url: "https://ke.jumia.is/cms/2023/W17/PartyHQ/TB/_TB_2-1.jpg" },
-	{ url: "https://ke.jumia.is/cms/2023/W17/PartyHQ/TB/_TB_2-1.jpg" },
-	{ url: "https://ke.jumia.is/cms/2023/W17/PartyHQ/TB/_TB_2-1.jpg" },
+	{ id: 1, url: "https://ke.jumia.is/cms/2023/W17/PartyHQ/TB/_TB_2-1.jpg" },
+	{ id: 2, url: "https://ke.jumia.is/cms/2023/W17/PartyHQ/TB/_TB_2-1.jpg" },
+	{ id: 3, url: "https://ke.jumia.is/cms/2023/W17/PartyHQ/TB/_TB_2-1.jpg" },
 ];
